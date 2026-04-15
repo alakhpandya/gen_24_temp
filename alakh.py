1b218979166b77d9ca0c0ed4814d778b516dc456
alakhpandya <79519053+alakhpandya@users.noreply.github.com>
2026-04-15 20:56:18 +0530

    Update alakh.py

--- alakh.py
+++ alakh.py
@@ -1,1 +1,7 @@
-print("Hello, Alakh Pandya this side, I just created this file in my branch!")
+# print("Hello, Alakh Pandya this side, I just created this file in my branch!")
+
+import numpy as np
+import pandas as pd
+
+print(np.__version__)
+print(pd.__version__)
